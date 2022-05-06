9949d605aabf691e46f0a15c67ec0bb05509336f
hossam <hussam.said42@gmail.com>
2022-05-06 07:32:37 +0200

    Angualr basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+import { IProduct } from './models/product';
+
+import { IPagination } from './models/pagination'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  products: IProduct[];
+
+  constructor(private http: HttpClient)
+  {
+
+  }
+
+  ngOnInit(): void {
+
+      this.http.get('http://localhost:7095/api/Products').subscribe((response:IPagination) => {
+        this.products = response.data;
+      });
+
+  }
+
   title = 'Skinet';
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import {enableProdMode} from '@angular/core';
+// enableProdMode();
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
